cf9d8931029935ef6d3659aeb2f70602673a4738
pawloshabtez3 <pawloshabtez@gmail.com>
2025-12-23 16:48:31 +0300

    Fix URL patterns and update post view to use get_object_or_404

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('post/<str:pk>', views.post, name='post')
-    ]
+    path('post/<int:pk>/', views.post, name='post'),
+]
--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
-from .models import Post 
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Post
+
+
 def index(request):
     posts = Post.objects.all()
     return render(request, 'index.html', {'posts': posts})
 
+
 def post(request, pk):
-    posts = Post.objects.get(id=pk)
-    return render(request, 'posts.html', {'posts': posts})
+    post_obj = get_object_or_404(Post, id=pk)
+    return render(request, 'posts.html', {'post': post_obj})
